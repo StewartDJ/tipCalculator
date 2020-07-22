736a41a085ed57554f4004f534d07515eda901b7
DJ Stewart <deshastewart1992@gmail.com>
2020-07-22 14:27:25 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+console.log("is linked")
+
+
+function calculateTip(){
+    var theBill = document.getElementById("bill").value
+    console.log(theBill);
+    
+    var theTip = document.getElementById("tipping").value
+    console.log(theTip);
+
+    var thePeople = document.getElementById("people").value
+    console.log(thePeople);
+
+    var yourTip = theTip * theBill;
+    console.log(yourTip)
+    var yourTotal = parseFloat(yourTip) + parseFloat(theBill);
+    console.log(yourTotal)
+    var youPay = parseFloat(yourTotal)/parseFloat(thePeople);
+    console.log(youPay)
+    var answer = youPay;
+
+    var postHTML = '<p> *You pay: $' + answer + ', yayA?</p>'
+   
+
+
+console.log(postHTML);
+document.getElementById('tipAmount').innerHTML = postHTML;
+}
+
+
